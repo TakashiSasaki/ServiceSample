4f3bcb346286f3209898ddb17178b083a3ffcf09
Takashi SASAKI <takashi316@gmail.com>
2023-01-25 11:57:11 +0900

    イベントリスナ書いた。

--- app/src/main/java/com/example/servicesample/MainActivity.java
+++ app/src/main/java/com/example/servicesample/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.servicesample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+    }//onCreate
+
+    public void onPlayButtonClick(View view) {
+        //サービスクラスにインテントを飛ばして再生を促す
+        Intent intent
+                = new Intent(MainActivity.this,
+                SoundManageService.class);
+        startService(intent);
+        findViewById(R.id.btPlay).setEnabled(false);
+        findViewById(R.id.btStop).setEnabled(true);
+    }//onPlayButtonClick
+
+    public void onStopButtonClick(View view) {
+        Intent intent = new Intent(MainActivity.this,
+                SoundManageService.class);
+        stopService(intent);
+        findViewById(R.id.btPlay).setEnabled(true);
+        findViewById(R.id.btStop).setEnabled(false);
+    }//onStopButtonClick
+
+}//MainActivity
